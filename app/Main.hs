0173413fb64d7681b4c193e44ea061b404b97be6
hjmtql <hjmtql@gmail.com>
2020-01-20 23:55:25 +0900

    add ledger outputs

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,43 @@
 module Main where
 
-import ClassyPrelude
+import           ClassyPrelude
 
 import qualified Csv
 import qualified Expenses
 import qualified Sales
+import           Business.Bookkeeping
 
 main :: IO ()
-main =
-  writeFileUtf8 "dist/transactions-2016.csv" $
-  Csv.fromTransactions $ do
-    Expenses.transactions 2016
-    Sales.transactions 2016
+main = do
+  -- 仕訳帳
+  writeFileUtf8 "dist/transactions-2016.csv" $ Csv.fromTransactions trans2016
+
+  -- 総勘定元帳
+  writeFileUtf8 "dist/chidaiyachin-2016.csv"
+    $ Csv.fromTransactionsToLedger "地代家賃" trans2016
+
+  writeFileUtf8 "dist/suidokonetsuhi-2016.csv"
+    $ Csv.fromTransactionsToLedger "水道光熱費" trans2016
+
+  writeFileUtf8 "dist/ryohikotsuhi-2016.csv"
+    $ Csv.fromTransactionsToLedger "旅費交通費" trans2016
+
+  writeFileUtf8 "dist/shomohinhi-2016.csv"
+    $ Csv.fromTransactionsToLedger "消耗品費" trans2016
+
+  writeFileUtf8 "dist/urikakekin-2016.csv"
+    $ Csv.fromTransactionsToLedger "売掛金" trans2016
+
+  writeFileUtf8 "dist/uriagedaka-2016.csv"
+    $ Csv.fromTransactionsToLedger "売上高" trans2016
+
+  writeFileUtf8 "dist/jigyonushikashi-2016.csv"
+    $ Csv.fromTransactionsToLedger "事業主貸" trans2016
+
+  writeFileUtf8 "dist/jigyonushikari-2016.csv"
+    $ Csv.fromTransactionsToLedger "事業主借" trans2016
+
+trans2016 :: Transactions
+trans2016 = do
+  Expenses.transactions 2016
+  Sales.transactions 2016
--- src/Csv.hs
+++ src/Csv.hs
@@ -1,37 +1,122 @@
 module Csv
   ( fromTransactions
-  ) where
+  , fromTransactionsToLedger
+  )
+where
 
-import Business.Bookkeeping
-import ClassyPrelude
+import           Business.Bookkeeping
+import           ClassyPrelude
+
+fromTransactionsToLedger :: Text -> Transactions -> Text
+fromTransactionsToLedger name =
+  unlines
+    . (transactionLedgerHeader :)
+    . fmap transactionLedgerBody
+    . mkLedgers name
+    . numberedTransactions
 
 fromTransactions :: Transactions -> Text
 fromTransactions =
-  unlines . (transactionHeader:) . fmap transactionBody . zip [1..] . runTransactions
+  unlines . (transactionHeader :) . fmap transactionBody . numberedTransactions
 
-transactionBody :: (Int, Journal) -> Text
-transactionBody (n, Journal {..}) =
-  intercalate ","
-    [ tshow n
-    , tshow $ formatTime defaultTimeLocale "%Y/%m/%d" tDay
-    , formatText . unCategoryName . cName . unDebitCategory $ tDebit
-    , formatText . unCategoryName . cName . unCreditCategory $ tCredit
-    , formatText . unDescription $ tDescription
-    , formatText . unSubDescription $ tSubDescription
-    , tshow $ unAmount tAmount
-    ]
+numberedTransactions :: Transactions -> [NumberedJournal]
+numberedTransactions = zip [1 ..] . runTransactions
+
+transactionBody :: NumberedJournal -> Text
+transactionBody (n, Journal {..}) = intercalate
+  ","
+  [ tshow n
+  , tshow $ formatTime defaultTimeLocale "%Y/%m/%d" tDay
+  , formatText . unCategoryName . cName . unDebitCategory $ tDebit
+  , formatText . unCategoryName . cName . unCreditCategory $ tCredit
+  , formatText . unDescription $ tDescription
+  , formatText . unSubDescription $ tSubDescription
+  , tshow $ unAmount tAmount
+  ]
+
+transactionLedgerBody :: LedgerRecord -> Text
+transactionLedgerBody LedgerRecord {..} = intercalate
+  ","
+  [ tshow jNo
+  , tshow $ formatTime defaultTimeLocale "%Y/%m/%d" jDay
+  , formatText . unCategoryName . cName $ jOpposite
+  , formatText . unDescription $ jDescription
+  , formatText . unSubDescription $ jSubDescription
+  , tshow $ unAmount jDebitAmount
+  , tshow $ unAmount jCreditAmount
+  ]
 
 formatText :: Text -> Text
 formatText = (<> "\"") . ("\"" <>)
 
 transactionHeader :: Text
-transactionHeader =
-  intercalate ","
-    [ formatText "取引No"
-    , formatText "取引日"
-    , formatText "借方勘定科目"
-    , formatText "貸方勘定科目"
-    , formatText "摘要"
-    , formatText "細目"
-    , formatText "貸借金額"
-    ]
+transactionHeader = intercalate
+  ","
+  [ formatText "取引No"
+  , formatText "取引日"
+  , formatText "借方勘定科目"
+  , formatText "貸方勘定科目"
+  , formatText "摘要"
+  , formatText "細目"
+  , formatText "貸借金額"
+  ]
+
+transactionLedgerHeader :: Text
+transactionLedgerHeader = intercalate
+  ","
+  [ formatText "取引No"
+  , formatText "取引日"
+  , formatText "相手方勘定科目"
+  , formatText "摘要"
+  , formatText "細目"
+  , formatText "借方金額"
+  , formatText "貸方金額"
+  ]
+
+data LedgerRecord = LedgerRecord
+  {  jNo :: Int
+  ,  jDay :: Day
+  ,  jDescription :: Description
+  ,  jSubDescription :: SubDescription
+  ,  jOpposite :: Category
+  ,  jDebitAmount :: Amount
+  ,  jCreditAmount :: Amount
+  }
+
+type NumberedJournal = (Int, Journal)
+
+debitLedgerRecord :: NumberedJournal -> LedgerRecord
+debitLedgerRecord (n, Journal {..}) = LedgerRecord
+  { jNo             = n
+  , jDay            = tDay
+  , jDescription    = tDescription
+  , jSubDescription = tSubDescription
+  , jOpposite       = unCreditCategory tCredit
+  , jDebitAmount    = tAmount
+  , jCreditAmount   = 0
+  }
+
+creditLedgerRecord :: NumberedJournal -> LedgerRecord
+creditLedgerRecord (n, Journal {..}) = LedgerRecord
+  { jNo             = n
+  , jDay            = tDay
+  , jDescription    = tDescription
+  , jSubDescription = tSubDescription
+  , jOpposite       = unDebitCategory tDebit
+  , jDebitAmount    = 0
+  , jCreditAmount   = tAmount
+  }
+
+mkLedgers :: Text -> [NumberedJournal] -> [LedgerRecord]
+mkLedgers name = mapMaybe (toLedgerRecord name)
+
+toLedgerRecord :: Text -> NumberedJournal -> Maybe LedgerRecord
+toLedgerRecord name nj@(_, j) = case j of
+  Journal { tDebit = DebitCategory (Category dName _), tCredit = CreditCategory (Category cName _) }
+    | unCategoryName cName == name
+    -> Just $ creditLedgerRecord nj
+    | unCategoryName dName == name
+    -> Just $ debitLedgerRecord nj
+    | otherwise
+    -> Nothing
+
